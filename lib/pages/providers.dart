4188304edc2d7988751a7b717a1dd4f4d175f4db
zackill5 <87101458+zackill5@users.noreply.github.com>
2022-05-26 09:27:35 -0500

    prueba, actualizacion del nombre providers

---
+++ lib/pages/providers.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Providers extends StatefulWidget {
+  const Providers({Key? key}) : super(key: key);
+
+  @override
+  State<Providers> createState() => _ProvidersState();
+}
+
+class _ProvidersState extends State<Providers> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
